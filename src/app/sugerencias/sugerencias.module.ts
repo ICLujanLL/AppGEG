04f3d3e0ce6f56676762f0c607003df86954bb7b
ICLujanLL <Nacional02*>
2019-03-09 10:15:16 -0400

    Declarar el servicio para poder usarlo en los componentes

--- src/app/sugerencias/sugerencias.module.ts
+++ src/app/sugerencias/sugerencias.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SugerenciasService } from './sugerencias.service';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    SugerenciasService
   ]
 })
 export class SugerenciasModule { }
