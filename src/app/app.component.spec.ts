d0ce2debf0b519b804ca662ce96c86dfcdc2ec9c
ICLujanLL <Nacional02*>
2019-03-08 20:01:21 -0400

    Adicon del menu principal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AppGEG!');
+    expect(compiled.querySelector('h1').textContent).toContain('GEG BOLIVIA Grupos de Educadores Google - Aprende, Comparte, Inspira y fortalece a los demás.');
   });
 });
