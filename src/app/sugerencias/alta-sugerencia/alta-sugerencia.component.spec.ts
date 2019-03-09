fce66c47ee3a4e74bff4671a85ceb51bb5c0dc0c
ICLujanLL <Nacional02*>
2019-03-09 10:50:48 -0400

    Crear componente que da de alta sugerencias

---
+++ src/app/sugerencias/alta-sugerencia/alta-sugerencia.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AltaSugerenciaComponent } from './alta-sugerencia.component';
+
+describe('AltaSugerenciaComponent', () => {
+  let component: AltaSugerenciaComponent;
+  let fixture: ComponentFixture<AltaSugerenciaComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AltaSugerenciaComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AltaSugerenciaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/sugerencias/alta-sugerencia/alta-sugerencia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alta-sugerencia',
+  templateUrl: './alta-sugerencia.component.html',
+  styleUrls: ['./alta-sugerencia.component.css']
+})
+export class AltaSugerenciaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
