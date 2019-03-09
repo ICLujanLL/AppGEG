bec3a4f15d9fb937b6ab37ed5fc1ecea81f503e0
ICLujanLL <Nacional02*>
2019-03-09 18:30:45 -0400

    alta-sugerencia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AltaSugerenciaComponent } from './sugerencias/alta-sugerencia/alta-sugerencia.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'alta-sugerencia', component: AltaSugerenciaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
