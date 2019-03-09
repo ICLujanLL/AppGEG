4456e21d1c8350c0734257a7a1390851dc2d12b0
ICLujanLL <Nacional02*>
2019-03-08 20:14:31 -0400

    Creamos el menu principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AppGEG';
+  title = 'GEG - Grupos de Educadores Google - Aprende, Comparte, Inspira y fortalece a los demás.';
 }
